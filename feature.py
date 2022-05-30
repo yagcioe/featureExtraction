6dcdf971c5b8bc6b5064e05ad635bf0af5bbf74a
oemer.yagci <yagcioe@gmail.com>
2022-05-30 16:02:58 +0200

    remove border from exported image

--- feature.py
+++ feature.py
@@ -1,9 +1,11 @@
+import math
+
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import hsv_to_rgb
-import stft
-import math
-import numpy as np
+
 import enviroment as env
+import stft
 
 
 def feature(wav):
@@ -11,10 +13,9 @@ def feature(wav):
     d1 = stft.transform(y1)
     d2 = stft.transform(y2)
 
-    ipd = ipdFeature(d1,d2)
-    iid = iidFeature(d1,d2)
-    intensity = intensityFeature(d1,d2)
-
+    ipd = ipdFeature(d1, d2)
+    iid = iidFeature(d1, d2)
+    intensity = intensityFeature(d1, d2)
 
     # print(intensity)
     # print(intensity.shape)
@@ -25,38 +26,41 @@ def feature(wav):
 
     # print(intensity[:,t])
     feature = np.dstack((ipd, iid, intensity))
-    
-    fig = plt.figure()
 
+    fig = plt.figure(frameon=False)
     feature = hsv_to_rgb(feature)
-    plt.imshow(feature, origin='lower',interpolation='none')
+    plt.imshow(feature, origin='lower', interpolation='none', aspect= 'auto')
     plt.axis('off')
     plt.close(fig)
     return fig
 
 
-def ipdFeature(D,E):
+def ipdFeature(D, E):
     diff = stft.to_phase(D)-stft.to_phase(E)
     return (boundAngle(diff) / (math.pi*2))
+
 
 def boundAngle(phi):
     return phi % (2*math.pi)
 
-def intensityFeature(D,E):
-    intensity = np.maximum(np.abs(D),np.abs(E))
+
+def intensityFeature(D, E):
+    intensity = np.maximum(np.abs(D), np.abs(E))
     intensity = stft.to_dB(intensity)
 
-    low= np.min(intensity)
-    high= np.max(intensity)
+    low = np.min(intensity)
+    high = np.max(intensity)
     diff = high-low
     intensity = (intensity-low) / diff
     return intensity
 
-def iidFeature(D,E):
+
+def iidFeature(D, E):
     return stft.to_iid(D, E) if env.iid else np.ones(D.shape)
+
 
 def exportFeature(fig, path):
     plt.figure(fig)
     plt.axis('off')
-    plt.savefig(path,bbox_inches='tight')
+    plt.savefig(path, bbox_inches='tight',pad_inches=0)
     plt.close(fig)
