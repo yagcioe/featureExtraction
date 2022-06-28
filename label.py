ccbd655fc3ae2766162c9217f85db0594020d5d3
oemer.yagci <yagcioe@gmail.com>
2022-06-28 22:04:36 +0200

    rm force

--- label.py
+++ label.py
@@ -23,7 +23,6 @@ def label(js):
 
         azimuth = speaker['direction']['azimuth']
         clas = simpleClassify(azimuth)
-        clas = 0# simple yes no detection instead of classes
         box = [clas, x, y, width, heigth]  # normalize to [0,1]
 
         boundingBoxes.append(box)
